12db091a2a8014f5fafda0add4d3e5be7d07e6e9
Pc_scuola <alessandro.aloise@samtrevano.ch>
2022-02-24 15:17:18 +0100

    modifica codice

--- EyeAsteroids/libreria/GazeTracking-master/example.py
+++ EyeAsteroids/libreria/GazeTracking-master/example.py
@@ -1,41 +1,42 @@
-"""
-Demonstration of the GazeTracking library.
-Check the README.md for complete documentation.
-"""
-
 import cv2
 from gaze_tracking import GazeTracking
 
 gaze = GazeTracking()
 webcam = cv2.VideoCapture(0)
+width = webcam.get(cv2.CAP_PROP_FRAME_WIDTH) 
+height = webcam.get(cv2.CAP_PROP_FRAME_HEIGHT )
 
 while True:
-    # We get a new frame from the webcam
     _, frame = webcam.read()
-
-    # We send this frame to GazeTracking to analyze it
+    frame = cv2.flip(frame, 1)
     gaze.refresh(frame)
 
+
     frame = gaze.annotated_frame()
-    text = ""
+    try:
+        #left_pupil = str(gaze.pupil_left_coords())
+        #right_pupil = str(gaze.pupil_right_coords())
+        #left_pupil =left_pupil[1:-1]
+        #right_pupil=right_pupil[1:-1]
 
-    if gaze.is_blinking():
-        text = "Blinking"
-    elif gaze.is_right():
-        text = "Looking right"
-    elif gaze.is_left():
-        text = "Looking left"
-    elif gaze.is_center():
-        text = "Looking center"
+        #array_left_pupil =left_pupil.split(",")
+        #array_right_pupil=right_pupil.split(",")
 
-    cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
+        # center_y= (int(array_left_pupil[1].strip())+int(array_right_pupil[1].strip()))/2
+        #center_x= (int(array_left_pupil[0].strip())+int(array_right_pupil[0].strip()))/2
 
-    left_pupil = gaze.pupil_left_coords()
-    right_pupil = gaze.pupil_right_coords()
-    cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-    cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+        
+        x = (gaze.horizontal_ratio())*width
+        y =(gaze.vertical_ratio())*height
+        print("x" +str(x))
+        print("y" + str(y))
+        cv2.circle(frame, (int(x), int(y)), 20, (255,0,0), 2)
+        cv2.putText(frame, "ratio x" + str(gaze.horizontal_ratio()), (20, 20), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+        cv2.putText(frame, "ratio y" + str(gaze.vertical_ratio()), (20, 50), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+    except:
+        print("occhi non rilevati")
 
-    cv2.imshow("Demo", frame)
+    cv2.imshow("Demo eye", frame)
 
     if cv2.waitKey(1) == 27:
         break
