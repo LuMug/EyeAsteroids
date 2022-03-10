c93d8860e4f3862fe0336b1df395a20df264745e
Alessandro Aloise <54986411+AlessandroAloise@users.noreply.github.com>
2022-03-10 11:53:13 +0100

    rimozione della tabella player

--- EyeAsteroids/space_asteroid/database.py
+++ EyeAsteroids/space_asteroid/database.py
@@ -4,16 +4,11 @@ import sqlite3
 def createDatabase():
    con = sqlite3.connect('./space_asteroid/score.db')
    cur = con.cursor()
-   player ='''CREATE TABLE IF NOT EXISTS player(
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      nome VARCHAR(25)
-   )'''
-   cur.execute(player)
    score ='''CREATE TABLE IF NOT EXISTS score(
        id INTEGER PRIMARY KEY AUTOINCREMENT, 
        score int,
        data date,
-       id_player int
+       nome varchar(25)
    )'''
    cur.execute(score)
    con.close()
