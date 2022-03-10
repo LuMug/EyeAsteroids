0083242f66102e67659cc98489dbe2844b601eaa
Alessandro Aloise <54986411+AlessandroAloise@users.noreply.github.com>
2022-03-10 11:43:11 +0100

    rinominato il file

---
+++ EyeAsteroids/space_asteroid/database.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+class Database:
+   def createDatabase():
+      con = sqlite3.connect('score.db')
+      cur = con.cursor()
+      player ='''CREATE TABLE  IF NOT EXISTS player(
+         id INTEGER PRIMARY KEY AUTOINCREMENT,
+         nome VARCHAR(25)
+      )'''
+      cur.execute(player)
+      score ='''CREATE TABLE  IF NOT EXISTS score(
+          id INTEGER PRIMARY KEY AUTOINCREMENT, 
+          score int,
+          data date,
+          id_player int
+      )'''
+      cur.execute(score)
+      con.close()
+
