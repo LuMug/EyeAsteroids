f19a71d222c030c857e491d3679ed8aa5d9d5880
mattiapasquini <mattia.pasquini@samtrevano.ch>
2022-03-10 12:12:28 +0100

    insert e show per db

--- EyeAsteroids/space_asteroid/database.py
+++ EyeAsteroids/space_asteroid/database.py
@@ -13,3 +13,23 @@ def createDatabase():
    cur.execute(score)
    con.close()
 
+def _insert_result(score, player):
+  conn = sqlite3.connect('./space_asteroid/score.db')
+  date = datetime.today().strftime('%Y-%m-%d')
+  conn.execute(f"INSERT INTO Score (score, data, nome) VALUES ({score}, {date}, {player})")
+  conn.commit()
+  conn.close()
+
+def _show_result():
+  conn = sqlite3.connect('./space_asteroid/score.db')
+  cor = cur.cursor()
+  cur.execute("SELECT * FROM Score")
+  rows = cur.fetchall()
+
+  for row in rows:
+      print(row)
+
+  conn.commit()
+  conn.close()
+
+
